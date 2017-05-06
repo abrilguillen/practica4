3555413a9c73aaf2e91ee713174a1998a03779c0
alan <alan@ALAN-PC>
2017-05-05 19:08:13 -0500

    ListasDobles

---
+++ _Formulario/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Formulario
+{
+    public partial class frmInventario : Form
+    {
+        Inventario inventario = new Inventario();
+
+        public frmInventario()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdAgregar_Click(object sender, EventArgs e)
+        {
+            int codigo = int.Parse(txtCodigo.Text);
+            string nombre = txtNombre.Text;
+            int cantidad = int.Parse(txtCantidad.Text);
+            double precio = Convert.ToDouble(txtPrecio.Text);
+
+            inventario.agregarProducto(new Producto(codigo, nombre, cantidad, precio));
+
+            txtCodigo.Clear();
+            txtCantidad.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void cmdEliminar_Click(object sender, EventArgs e)
+        {
+            int codigo = int.Parse(txtBuscarXNombre.Text);
+            inventario.eliminar(codigo);
+        }
+
+        private void cmdInsertar_Click(object sender, EventArgs e)
+        {
+            int codigo = int.Parse(txtCodigo.Text);
+            string nombre = txtNombre.Text;
+            int cantidad = int.Parse(txtCantidad.Text);
+            double precio = Convert.ToDouble(txtPrecio.Text);
+            int posicion = int.Parse(txtPosicion.Text);
+
+            inventario.insertar(new Producto(codigo, nombre, cantidad, precio), posicion);
+        }
+
+        private void cmdBuscar_Click(object sender, EventArgs e)
+        {
+            
+            Producto producto = inventario.buscar(int.Parse(txtBuscarXNombre.Text));
+
+            if (producto != null)
+            {
+                txtReporte.Text = producto.ToString();
+            }
+            else
+            {
+                MessageBox.Show("El producto quee indica no se encontro");
+            }
+        }
+
+        private void cmdReporte_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = inventario.reporte();
+        }
+
+        private void cmdAgregarInicio_Click(object sender, EventArgs e)
+        {
+            int codigo = int.Parse(txtCodigo.Text);
+            string nombre = txtNombre.Text;
+            int cantidad = int.Parse(txtCantidad.Text);
+            double precio = Convert.ToDouble(txtPrecio.Text);
+            //se pasan los parametros
+            inventario.agregarProductoInicio(new Producto(codigo, nombre, cantidad, precio));
+
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtCantidad.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void cmdEliminarInicio_Click(object sender, EventArgs e)
+        {
+            //int codigo = int.Parse(txtBuscarXNombre.Text);
+            inventario.eliminarInicio();
+        }
+
+        private void cmdEliminarUltimo_Click(object sender, EventArgs e)
+        {
+            inventario.eliminarUltimo();
+        }
+
+        private void cmdReporteInverso_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = inventario.reporteInverso();
+        }
+    }
+}
---
+++ _Formulario/Inventario.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Formulario
+{
+    ////////////////////////LISTAS DOBLES////////////////////////////////////
+    class Inventario
+    {
+        private Producto inicio;
+
+        //almacena el indice que tienen los productos (numero de productos)
+        private int _indiceProducto;
+        
+        public Inventario()
+        {
+            _indiceProducto = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Total de productos: " + _indiceProducto;
+        }
+
+        public void agregarProducto(Producto producto)
+        {
+            
+            if (inicio == null)
+            {
+                inicio = producto;
+            }
+            else
+            {
+                //el anterior del iniicio no tiene nada, El puntero anterior del primer elemento debe apuntar hacia NULL
+                Producto temporal = inicio;
+
+                while (temporal.siguiente != null)
+                {
+                    temporal = temporal.siguiente;
+                }
+
+                producto.anterior = temporal;
+                temporal.siguiente = producto;
+            }
+            _indiceProducto++;
+            
+        }
+
+        public void agregarProductoInicio(Producto producto)
+        {
+            //validar que inicio sea diferente a null
+            if (inicio != null)
+            {
+                //teniendo A.B.C y queremos agregar G al principio
+                //El anterior de A = G
+                inicio.anterior = producto;
+                //El siguiente de G es A
+                producto.siguiente = inicio;
+                inicio = producto;
+            }
+            _indiceProducto++;
+        }
+
+        // RECURSIVIDAD
+        //public void agregarProducto( Producto nuevo)
+        //{
+        //    if (inicio == null)
+        //    {
+        //        inicio = nuevo;
+        //        _indiceProducto++;
+        //    }
+        //    else
+        //        agregarProducto(nuevo, inicio);
+        //}
+
+        //private void agregarProducto(Producto nuevo, Producto ultimo)
+        //{
+        //    if(ultimo.siguiente == null)
+        //    {
+        //        ultimo.siguiente = nuevo;
+        //        _indiceProducto++;
+        //    }
+
+        //    else
+        //    {
+        //        agregarProducto(nuevo, ultimo.siguiente);
+        //    }
+        //}
+
+        public Producto buscar(int codigo)
+        {
+            Producto temporal = inicio;
+
+            while (temporal != null)
+            {
+                if(temporal.codigoDeBarras == codigo)
+                {
+                    return temporal;
+                }
+
+                else
+                {
+                    temporal = temporal.siguiente;
+                }
+            }
+            return null;
+        }
+
+        public void eliminarInicio()
+        {
+            if (inicio != null)
+            {
+                if (inicio.siguiente != null)
+                {
+                    inicio.siguiente.anterior = null;
+                }
+
+                inicio = inicio.siguiente;
+                //
+                _indiceProducto--;
+            }
+        }
+
+        public void eliminarUltimo()
+        {
+            Producto temporal = inicio;
+
+            while(temporal.siguiente != null)
+            {
+                temporal = temporal.siguiente;
+                
+            }
+            temporal.anterior.siguiente = null;
+        }
+
+        public void eliminar(int codigo)
+        {
+            if(inicio != null)
+            {
+                if(inicio.codigoDeBarras == codigo)
+                {
+                    // A.B
+                    //para eliminar la coneccion de B a A
+                    if(inicio.siguiente != null)
+                    {
+                        inicio.siguiente.anterior = null;
+                    }
+
+                    inicio = inicio.siguiente;
+                    //A = B
+                    _indiceProducto--;
+                }
+                else
+                {
+                    //uso del método buscar, busca el código que se quiere eliminar
+                    Producto temporal = buscar(codigo);
+                    //eliminar el producto de en medio y eliminar un producto al ultimo
+                    //no es necesario romper el enlace anterior en el ultimo producto para eliminarlo
+
+                    if(temporal != null)
+                    {
+                        if(temporal.siguiente != null)
+                        {
+                            
+                            temporal.siguiente.anterior = temporal.anterior;
+                        }
+
+                        temporal.anterior.siguiente = temporal.siguiente;
+
+                        _indiceProducto--;
+                    }
+
+                }
+            }
+
+        }
+
+
+        public void insertar(Producto producto, int posicion)
+            //se inserta un producto dandole la posicion en la que quieres que se inserte
+            //el ultimo elemento no se pierde, solo se recorre
+            //el indice empieza desde 0, 0=A, 1=C...
+            //si tienes A.C.D y quieres insertar B en la posicion 1 queda asi: 0,1,2,3
+            //                                                                 A.B.C.D
+        {   
+            if(inicio != null && posicion >= 0 && posicion < _indiceProducto)
+            {
+                //si quiero insertar en a posicion 0
+                if (posicion == 0)
+                {
+                    producto.siguiente = inicio;
+                    inicio.anterior = producto;
+                    inicio = producto;
+                }
+                //si quiero insertar en una posicion despues de 0 que se encuentre en medio (pos 5)
+                //o para insertar en la ultima posicion
+                else
+                {
+                    Producto temporal = inicio;
+                    int indice = 0;
+                    //ciclo que va hasta la posicion que le pongo para insertar 
+                    //insertar en posicion 5 (teniendo 8 objetos, asi ya no se recorren todos, solo hasta 5)
+                    while (indice != posicion)
+                    {
+                        temporal = temporal.siguiente;
+                        indice++;
+                    }
+                    //tengo A.C.D y quiero insertar B
+                    //quedaría A.B.C.D
+                    //temporal debe tener el valor de la posicion que quiero insertar = B
+
+                    // mi producto es B (porque es el producto que se inserta) apunta a C    = C
+                    //B.C
+                    producto.siguiente = temporal;
+                    // A      = B
+                    producto.anterior = temporal.anterior;
+                    temporal.anterior.siguiente = producto;
+                    temporal.anterior = producto;
+                }
+                _indiceProducto++;
+            }
+        }
+
+        public string reporte()
+        { 
+            string mostrar = ToString() + Environment.NewLine;
+            Producto temporal = inicio;
+
+            while( temporal != null)
+            {
+                mostrar += "--------------------------------" + Environment.NewLine;
+                mostrar += temporal.ToString();
+                temporal = temporal.siguiente;
+            }
+
+            return mostrar;
+        }
+
+        public string reporteInverso()
+        {
+            string mostrar = "";
+            Producto temporal = inicio;
+            while (temporal != null)
+            {
+                //
+                mostrar = temporal.ToString() + Environment.NewLine + mostrar;
+                temporal = temporal.siguiente;
+
+            }
+            //1,2,3,4 ahora que lo muestre 4,3,2,1
+            return mostrar;
+        }
+    }
+}
